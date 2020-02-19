444fc629452e8894049ac9e63cfef279326b515d
Fredrik Hoem Grelland <fredrikhoem.grelland@skatteetaten.no>
2020-02-19 18:36:17 +0100

    dsa

--- docker/hive/init-hive-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-#
-cd /tmp
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE USER hive WITH PASSWORD 'hive';
-  CREATE DATABASE metastore;
-  GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
-EOSQL
